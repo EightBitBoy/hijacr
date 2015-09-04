506f4a0ca6462de2095bb4d7106bee975c305c2e
Stefan Haertel <haertel.dev@gmail.com>
2015-09-04 22:58:40 +0200

    Adjust layout, add Jsoup dependency

--- app/src/main/java/de/eightbitboy/hijacr/MainActivity.java
+++ app/src/main/java/de/eightbitboy/hijacr/MainActivity.java
@@ -1,9 +1,12 @@
 package de.eightbitboy.hijacr;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -33,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //TODO clean up all this logic
+    private void setUpButtonActions() {
+
+
+        ImageView comicView = (ImageView) findViewById(R.id.comic_view)
+        Button olderButton = (Button) findViewById(R.id.older_button);
+        Button newerButton = (Button) findViewById(R.id.newer_button);
+
+        newerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
