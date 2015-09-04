eae250310f02e9298ebfd9f8f5c7c41347bc8a9b
Stefan Haertel <haertel.dev@gmail.com>
2015-09-04 21:56:22 +0200

    Play with test

--- app/src/androidTest/java/de/eightbitboy/hijacr/ApplicationTest.java
+++ app/src/androidTest/java/de/eightbitboy/hijacr/ApplicationTest.java
@@ -3,11 +3,17 @@ package de.eightbitboy.hijacr;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import junit.framework.Assert;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testSomething() {
+        Assert.assertEquals("foo", "bar");
     }
 }
