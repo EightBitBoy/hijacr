84d432c396297eeccee5993ceeb2eed0152e0a0e
Stefan Haertel <haertel.dev@gmail.com>
2015-09-07 13:23:56 +0200

    Load layout

--- app/src/main/java/de/eightbitboy/hijacr/fragments/ComicListFragment.java
+++ app/src/main/java/de/eightbitboy/hijacr/fragments/ComicListFragment.java
@@ -1,6 +1,19 @@
 package de.eightbitboy.hijacr.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.eightbitboy.hijacr.R;
 
 public class ComicListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.comic_list, container, false);
+        return view;
+    }
 }
--- app/src/main/java/de/eightbitboy/hijacr/fragments/ComicViewerFragment.java
+++ app/src/main/java/de/eightbitboy/hijacr/fragments/ComicViewerFragment.java
@@ -1,7 +1,19 @@
 package de.eightbitboy.hijacr.fragments;
 
-/**
- * Created by shaertel on 07.09.2015.
- */
-public class ComicViewerFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.eightbitboy.hijacr.R;
+
+public class ComicViewerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.comic_viewer, container, false);
+        return view;
+    }
 }
