93bca7286f2a3d113e56163b1575b71bb1e48c75
Stefan Haertel <haertel.dev@gmail.com>
2015-09-08 11:00:49 +0200

    Rename layout file

--- app/src/main/java/de/eightbitboy/hijacr/MainActivity.java
+++ app/src/main/java/de/eightbitboy/hijacr/MainActivity.java
@@ -3,54 +3,43 @@ package de.eightbitboy.hijacr;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
-import com.orhanobut.logger.Logger;
-
-import butterknife.Bind;
 import butterknife.ButterKnife;
-import de.eightbitboy.hijacr.data.ComicFetcher;
 import de.eightbitboy.hijacr.fragments.PagerFragmentAdpater;
 
 public class MainActivity extends FragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        //TODO move this to application class
-        Logger.init("foo");
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+		setContentView(R.layout.hijacr_main);
+		ButterKnife.bind(this);
 
+		ViewPager pager = (ViewPager) findViewById(R.id.vpPager);
+		pager.setAdapter(new PagerFragmentAdpater(getSupportFragmentManager()));
+	}
 
-        ViewPager pager = (ViewPager) findViewById(R.id.vpPager);
-        pager.setAdapter(new PagerFragmentAdpater(getSupportFragmentManager()));
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+		return super.onOptionsItemSelected(item);
+	}
 }
