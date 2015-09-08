2b223e9e5c77bf55dcee6d502222fd768069c219
Stefan Haertel <haertel.dev@gmail.com>
2015-09-08 08:44:34 +0200

    Adjust code style

--- app/src/main/java/de/eightbitboy/hijacr/data/ComicRepository.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/ComicRepository.java
@@ -1,23 +1,25 @@
 package de.eightbitboy.hijacr.data;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ComicRepository {
-    private Map<String, ComicData> comics = new HashMap<String,ComicData>();
+	private Map<String, ComicData> comics = new HashMap<String, ComicData>();
 
-    public ComicRepository(){
-        initializeComics();
-    }
+	public ComicRepository() {
+		initializeComics();
+	}
 
-    private void initializeComics(){
-        comics.put("xkcd", new ComicData("XKCD", "http://xkcd.com/", "http://xkcd.com/1/", "div[id=comic] img[src]", "ul[class=comicNav] a[rel=next]", "ul[class=comicNav] a[rel=prev]"));
-        comics.put("extralife", new ComicData("ExtraLife", "http://www.myextralife.com/", "http://www.myextralife.com/comic/06172001/", "div[class=comic_container] img[src]", "a[class=next_comic_link]", "a[class=prev_comic_link]"));
-    }
+	private void initializeComics() {
+		comics.put("xkcd", new ComicData("XKCD", "http://xkcd.com/", "http://xkcd.com/1/",
+				"div[id=comic] img[src]", "ul[class=comicNav] a[rel=next]",
+				"ul[class=comicNav] a[rel=prev]"));
+		comics.put("extralife", new ComicData("ExtraLife", "http://www.myextralife.com/",
+				"http://www.myextralife.com/comic/06172001/", "div[class=comic_container] img[src]",
+				"a[class=next_comic_link]", "a[class=prev_comic_link]"));
+	}
 
-    public ComicData getComicData(String key){
-        return comics.get(key);
-    }
+	public ComicData getComicData(String key) {
+		return comics.get(key);
+	}
 }
