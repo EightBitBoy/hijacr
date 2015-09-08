ab7c968e093ac876cb6b836328f377409554a72f
Stefan Haertel <haertel.dev@gmail.com>
2015-09-08 10:00:01 +0200

    Adapter functionality

--- app/src/main/java/de/eightbitboy/hijacr/fragments/ComicListAdapter.java
+++ app/src/main/java/de/eightbitboy/hijacr/fragments/ComicListAdapter.java
@@ -1,4 +1,33 @@
 package de.eightbitboy.hijacr.fragments;
 
-public class ComicListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import de.eightbitboy.hijacr.R;
+import de.eightbitboy.hijacr.data.ComicData;
+
+public class ComicListAdapter extends ArrayAdapter<ComicData> {
+	private Context context;
+	private List<ComicData> comics;
+
+	public ComicListAdapter(Context context, int resource, List<ComicData> objects) {
+		//TODO find out what resource does, fix -1 below
+		super(context, -1, objects);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context
+				.LAYOUT_INFLATER_SERVICE);
+		View view = inflater.inflate(R.layout.comic_list_layout, parent, false);
+		TextView title = (TextView) view.findViewById(R.id.comic_title);
+		title.setText(comics.get(position).getTitle());
+		return view;
+	}
 }
