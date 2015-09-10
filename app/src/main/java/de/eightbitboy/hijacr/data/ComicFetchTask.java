ba92a10894b2ebc284f39abfcadcc1b8acdadc39
Stefan Haertel <haertel.dev@gmail.com>
2015-09-10 11:53:51 +0200

    AsyncTask fiddle

--- app/src/main/java/de/eightbitboy/hijacr/data/ComicFetchTask.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/ComicFetchTask.java
@@ -2,12 +2,38 @@ package de.eightbitboy.hijacr.data;
 
 import android.os.AsyncTask;
 
-/**
- * Created by Stefan on 08.09.2015.
- */
-public class ComicFetchTask extends AsyncTask {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class ComicFetchTask extends AsyncTask<Void, Void, String> {
+
+	private String targetUrl;
+	private String imageQuery;
+
+	public ComicFetchTask(String baseUrl, int number, String imageQuery) {
+		this.targetUrl = baseUrl + number;
+		this.imageQuery = imageQuery;
+	}
+
 	@Override
-	protected Object doInBackground(Object[] params) {
+	protected String doInBackground(Void... voids) {
+		try {
+			Document page = Jsoup.connect(targetUrl).get();
+			Elements image = page.select(imageQuery);
+			return image.attr("src");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
+	}
+
+	@Override
+	protected void onPostExecute(String s) {
+		//TODO
+		super.onPostExecute(s);
 	}
 }
--- app/src/main/java/de/eightbitboy/hijacr/data/ComicManager.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/ComicManager.java
@@ -59,4 +59,6 @@ public class ComicManager {
 		ImageLoader.getInstance().displayImage(url, comicView);
 	}
 	*/
+
+
 }
