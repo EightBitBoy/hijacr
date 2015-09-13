c7daf99fe14d682b6ba03921992e2a9bd7ec3197
Stefan Haertel <haertel.dev@gmail.com>
2015-09-14 00:52:01 +0200

    Add contract class

---
+++ app/src/main/java/de/eightbitboy/hijacr/data/database/ComicDatabaseContract.java
@@ -0,0 +1,12 @@
+package de.eightbitboy.hijacr.data.database;
+
+import android.provider.BaseColumns;
+
+public class ComicDatabaseContract {
+	public ComicDatabaseContract() {
+	}
+
+	public static abstract class ComicDataEntry implements BaseColumns {
+
+	}
+}
