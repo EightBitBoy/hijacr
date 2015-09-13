0b4fea37f856906a7d1fe26a8ead2089ed7eaa29
Stefan Haertel <haertel.dev@gmail.com>
2015-09-14 01:12:44 +0200

    Adjust contract

--- app/src/main/java/de/eightbitboy/hijacr/data/database/ComicDatabaseContract.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/database/ComicDatabaseContract.java
@@ -3,10 +3,20 @@ package de.eightbitboy.hijacr.data.database;
 import android.provider.BaseColumns;
 
 public class ComicDatabaseContract {
+
 	public ComicDatabaseContract() {
 	}
 
 	public static abstract class ComicDataEntry implements BaseColumns {
+		public static final String TABLE_NAME = "comicdata";
+		public static final String COLUMN_TITLE = "title";
+		public static final String COLUMN_URL = "url";
+		public static final String COLUMN_FIRST_URL = "firsturl";
+		public static final String COLUMN_BASE_URL = "baseurl";
+		public static final String COLUMN_NEXT_QUERY = "nextquery";
+		public static final String COLUMN_PREV_QUERY = "prevquery";
+		public static final String COLUMN_IMAGE_QUERY = "imagequery";
 
+		//TODO add some statements for creating and deleting table
 	}
 }
