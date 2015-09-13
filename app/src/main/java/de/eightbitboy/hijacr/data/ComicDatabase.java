32e3d0a4e69074655f650eeb892b497961c3318b
Stefan Haertel <haertel.dev@gmail.com>
2015-09-13 18:44:35 +0200

    Extend SQLiteOpenHelper

--- app/src/main/java/de/eightbitboy/hijacr/data/ComicDatabase.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/ComicDatabase.java
@@ -1,7 +1,26 @@
 package de.eightbitboy.hijacr.data;
 
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Stores information about comics and reading progress in a SQLite database.
  */
-public class ComicDatabase {
+public class ComicDatabase extends SQLiteOpenHelper {
+	public ComicDatabase(Context context, String name, SQLiteDatabase.CursorFactory factory,
+			int version, DatabaseErrorHandler errorHandler) {
+		super(context, name, factory, version, errorHandler);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
 }
