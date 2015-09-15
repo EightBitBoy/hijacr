f227eb90da0e569d8e21630a53a522a15f791c1c
Stefan Haertel <haertel.dev@gmail.com>
2015-09-15 14:46:03 +0200

    Add database statement

--- app/src/main/java/de/eightbitboy/hijacr/data/database/ComicDatabaseContract.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/database/ComicDatabaseContract.java
@@ -8,12 +8,20 @@ public class ComicDatabaseContract {
 	}
 
 	public static abstract class ComicDataEntry implements BaseColumns {
+		private static final String TEXT_TYPE = " TEXT";
+		private static final String COMMA_SEP = ",";
+
 		public static final String TABLE_NAME = "comicdata";
 		public static final String COLUMN_KEY = "title";
 		public static final String COLUMN_PROGRESS_URL = "progressurl";
 		public static final String COLUMN_PROGRESS_NUMBER = "progressnumber";
 
-		//TODO add some statements for creating and deleting table
-		public static final String CREATE_TABLE = "";
+		public static final String CREATE_TABLE =
+				"CREATE TABLE " + TABLE_NAME + " (" +
+						_ID + " INTEGER PRIMARY KEY," +
+						COLUMN_KEY + TEXT_TYPE + COMMA_SEP +
+						COLUMN_PROGRESS_URL + TEXT_TYPE + COMMA_SEP + " )";
+
+		public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 	}
 }
