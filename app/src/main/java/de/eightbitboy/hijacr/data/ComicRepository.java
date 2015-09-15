9f2ee69a0fb9d8ef94308e1e51eddd41c4eb684d
Stefan Haertel <haertel.dev@gmail.com>
2015-09-15 13:29:12 +0200

    Support ExtraLife comic

--- app/src/main/java/de/eightbitboy/hijacr/data/ComicRepository.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/ComicRepository.java
@@ -26,6 +26,9 @@ public class ComicRepository {
 
 	//TODO a comic might start at position 0 like vgcats!
 	private void initializeComics() {
+		comics.put("extralife", new ComicData("ExtraLife", "http://www.myextralife.com/",
+				"http://www.myextralife.com/comic/06172001/", ".comic", ".prev_comic_link",
+				".next_comic_link"));
 		comics.put("smbc", new ComicData("SMBC", "http://www.smbc-comics.com/",
 				"http://www.smbc-comics.com/index.php?id=", 1, "#comic"));
 		comics.put("vgcats",
