f5c0efaf327c585274364fa94219f8fee0fdc6ae
Stefan Haertel <haertel.dev@gmail.com>
2015-09-15 14:40:10 +0200

    Simple database design for storing progress

--- app/src/main/java/de/eightbitboy/hijacr/data/database/ComicDatabaseContract.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/database/ComicDatabaseContract.java
@@ -9,14 +9,11 @@ public class ComicDatabaseContract {
 
 	public static abstract class ComicDataEntry implements BaseColumns {
 		public static final String TABLE_NAME = "comicdata";
-		public static final String COLUMN_TITLE = "title";
-		public static final String COLUMN_URL = "url";
-		public static final String COLUMN_FIRST_URL = "firsturl";
-		public static final String COLUMN_BASE_URL = "baseurl";
-		public static final String COLUMN_NEXT_QUERY = "nextquery";
-		public static final String COLUMN_PREV_QUERY = "prevquery";
-		public static final String COLUMN_IMAGE_QUERY = "imagequery";
+		public static final String COLUMN_KEY = "title";
+		public static final String COLUMN_PROGRESS_URL = "progressurl";
+		public static final String COLUMN_PROGRESS_NUMBER = "progressnumber";
 
 		//TODO add some statements for creating and deleting table
+		public static final String CREATE_TABLE = "";
 	}
 }
