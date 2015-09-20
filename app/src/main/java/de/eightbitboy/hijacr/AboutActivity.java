e87478555c577d3871a82f13d9c2df519250a3eb
Stefan Haertel <haertel.dev@gmail.com>
2015-09-20 15:01:12 +0200

    Html fiddle, add dependency

--- app/src/main/java/de/eightbitboy/hijacr/AboutActivity.java
+++ app/src/main/java/de/eightbitboy/hijacr/AboutActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.widget.TextView;
 
+import org.sufficientlysecure.htmltextview.HtmlTextView;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,6 +30,10 @@ public class AboutActivity extends AppCompatActivity {
 
 		textView = (TextView) findViewById(R.id.changelog_text);
 		textView.setText(Html.fromHtml(readFromFile("changelog.html")));
+
+		HtmlTextView htmlTextView = (HtmlTextView) findViewById(R.id.html_text);
+		htmlTextView.setHtmlFromString(readFromFile("about.html"),
+				new HtmlTextView.LocalImageGetter());
 	}
 
 	private String readFromFile(String fileName) {
