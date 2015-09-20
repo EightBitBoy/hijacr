47df72f01f58837150018d100fcc8593f42a7038
Stefan Haertel <haertel.dev@gmail.com>
2015-09-20 03:03:18 +0200

    Fiddle for getting asset

--- app/src/main/java/de/eightbitboy/hijacr/AboutActivity.java
+++ app/src/main/java/de/eightbitboy/hijacr/AboutActivity.java
@@ -2,6 +2,14 @@ package de.eightbitboy.hijacr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.orhanobut.logger.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class AboutActivity extends AppCompatActivity {
 	@Override
@@ -9,5 +17,27 @@ public class AboutActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_about);
+
+		try {
+			StringBuilder builder = new StringBuilder();
+			InputStream inputStream = getApplicationContext().getAssets().open("about.html");
+			//inputStream = getApplicationContext().getResources().openRawResource(R.raw.about);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+			String s;
+			while ((s = reader.readLine()) != null) {
+				builder.append(s);
+			}
+
+			reader.close();
+			inputStream.close();
+
+			TextView textView = (TextView) findViewById(R.id.about_text);
+
+			Logger.wtf(s);
+			textView.setText(s);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
