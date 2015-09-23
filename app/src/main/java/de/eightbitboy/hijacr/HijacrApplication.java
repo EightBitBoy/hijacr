608364aeba7ab3808b58189c6267e66aecaf979e
Stefan Haertel <haertel.dev@gmail.com>
2015-09-23 14:32:32 +0200

    Add support for fabric.io crashlytics

--- app/src/main/java/de/eightbitboy/hijacr/HijacrApplication.java
+++ app/src/main/java/de/eightbitboy/hijacr/HijacrApplication.java
@@ -2,13 +2,16 @@ package de.eightbitboy.hijacr;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import io.fabric.sdk.android.Fabric;
 
 public class HijacrApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		Fabric.with(this, new Crashlytics());
 
 		//TODO enable disk caching and configure other stuff
 		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
