e7ac498f4e2375d46a9c79f94645054961a3f74a
Stefan Haertel <haertel.dev@gmail.com>
2015-09-25 01:38:45 +0200

    Add DebugActivity

---
+++ app/src/main/java/de/eightbitboy/hijacr/DebugActivity.java
@@ -0,0 +1,11 @@
+package de.eightbitboy.hijacr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DebugActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
