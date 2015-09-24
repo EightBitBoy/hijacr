ced124a08f8c87723298a38f368050926ab38a4e
Stefan Haertel <haertel.dev@gmail.com>
2015-09-25 01:47:50 +0200

    Show debug activity

--- app/src/main/java/de/eightbitboy/hijacr/MainActivity.java
+++ app/src/main/java/de/eightbitboy/hijacr/MainActivity.java
@@ -86,6 +86,11 @@ public class MainActivity extends AppCompatActivity {
 			return true;
 		}
 
+		if (id == R.id.action_debug) {
+			startActivity(new Intent(this, DebugActivity.class));
+			return true;
+		}
+
 		return super.onOptionsItemSelected(item);
 	}
 
