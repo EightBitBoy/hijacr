fe77603781059359ab1dcab56b0da1f2134dd86a
Stefan Haertel <haertel.dev@gmail.com>
2015-09-25 01:27:31 +0200

    SettingsManager is a singleton

--- app/src/main/java/de/eightbitboy/hijacr/data/SettingsManager.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/SettingsManager.java
@@ -1,4 +1,22 @@
 package de.eightbitboy.hijacr.data;
 
+import android.content.Context;
+
 public class SettingsManager {
+
+	private static SettingsManager instance;
+
+	private Context context;
+
+	private SettingsManager(Context context) {
+		this.context = context;
+	}
+
+	public static SettingsManager getInstance(Context context) {
+		if (instance == null) {
+			instance = new SettingsManager(context);
+		}
+
+		return instance;
+	}
 }
