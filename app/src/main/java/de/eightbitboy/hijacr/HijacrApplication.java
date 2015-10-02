0b1daf31680a67144a464722458b766ea6367f35
Stefan Haertel <haertel.dev@gmail.com>
2015-10-02 22:13:29 +0200

    Enable disk and memory caching

--- app/src/main/java/de/eightbitboy/hijacr/HijacrApplication.java
+++ app/src/main/java/de/eightbitboy/hijacr/HijacrApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.crashlytics.android.Crashlytics;
 import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
 import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
@@ -17,12 +18,19 @@ public class HijacrApplication extends Application {
 
 		Fabric.with(this, new Crashlytics());
 
+		DisplayImageOptions options = new DisplayImageOptions.Builder()
+				.cacheInMemory(true)
+				.cacheOnDisk(true)
+						//TODO experiment with bitmap config
+						//.bitmapConfig(Bitmap.Config.RGB_565)
+				.build();
+
 		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
 				.memoryCacheExtraOptions(2048, 2048)
 						//TODO use getFilesDir?
 				.diskCache(new UnlimitedDiskCache(getCacheDir()))
 				.diskCacheFileNameGenerator(new HashCodeFileNameGenerator())
-						//.defaultDisplayImageOptions()
+				.defaultDisplayImageOptions(options)
 				.writeDebugLogs()
 				.build();
 
