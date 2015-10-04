11d8669c8d67e8d622f41872b565bc65f94f80c6
Stefan Haertel <haertel.dev@gmail.com>
2015-10-04 23:16:15 +0200

    Implement view holder pattern

--- app/src/main/java/de/eightbitboy/hijacr/fragments/ComicListAdapter.java
+++ app/src/main/java/de/eightbitboy/hijacr/fragments/ComicListAdapter.java
@@ -25,16 +25,31 @@ public class ComicListAdapter extends ArrayAdapter<ComicData> {
 	//TODO implement view holder pattern
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context
-				.LAYOUT_INFLATER_SERVICE);
-		View view = inflater.inflate(R.layout.comic_list_item, parent, false);
+		ComicData data = comics.get(position);
 
-		TextView title = (TextView) view.findViewById(R.id.comic_title);
-		title.setText(comics.get(position).getTitle());
+		ViewHolder viewHolder;
+		if (convertView == null) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+					Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.comic_list_item, parent, false);
 
-		TextView url = (TextView) view.findViewById(R.id.comic_url);
-		url.setText(comics.get(position).getCleanUrl());
+			viewHolder = new ViewHolder();
+			viewHolder.title = (TextView) convertView.findViewById(R.id.comic_title);
+			viewHolder.url = (TextView) convertView.findViewById(R.id.comic_url);
 
-		return view;
+			convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
+
+		viewHolder.title.setText(data.getTitle());
+		viewHolder.url.setText(data.getCleanUrl());
+
+		return convertView;
+	}
+
+	private class ViewHolder {
+		public TextView title;
+		public TextView url;
 	}
 }
