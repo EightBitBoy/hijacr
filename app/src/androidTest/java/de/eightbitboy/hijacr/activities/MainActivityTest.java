fc49129d8cc8637e51bcc55fb989073e2834b2c9
Stefan Haertel <haertel.dev@gmail.com>
2015-10-13 10:07:37 +0200

    Adjust import

--- app/src/androidTest/java/de/eightbitboy/hijacr/activities/MainActivityTest.java
+++ app/src/androidTest/java/de/eightbitboy/hijacr/activities/MainActivityTest.java
@@ -12,7 +12,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
