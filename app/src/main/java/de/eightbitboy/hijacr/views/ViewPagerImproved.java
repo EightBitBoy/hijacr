6974b93eeef9d7917ef86d977793c31c74c5747a
Stefan Haertel <haertel.dev@gmail.com>
2015-10-16 20:30:45 +0200

    Add custom ViewPager for fixing crash

---
+++ app/src/main/java/de/eightbitboy/hijacr/views/ViewPagerImproved.java
@@ -0,0 +1,16 @@
+package de.eightbitboy.hijacr.views;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+public class ViewPagerImproved extends ViewPager {
+
+	public ViewPagerImproved(Context context) {
+		super(context);
+	}
+
+	public ViewPagerImproved(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
