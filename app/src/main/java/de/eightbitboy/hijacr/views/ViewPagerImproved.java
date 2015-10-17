dc5bd273f51576239ad8ab71e9593f9da7b46424
Stefan Haertel <haertel.dev@gmail.com>
2015-10-17 22:40:22 +0200

    Fix crash while zooming

--- app/src/main/java/de/eightbitboy/hijacr/views/ViewPagerImproved.java
+++ app/src/main/java/de/eightbitboy/hijacr/views/ViewPagerImproved.java
@@ -3,6 +3,9 @@ package de.eightbitboy.hijacr.views;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.orhanobut.logger.Logger;
 
 public class ViewPagerImproved extends ViewPager {
 
@@ -12,5 +15,15 @@ public class ViewPagerImproved extends ViewPager {
 
 	public ViewPagerImproved(Context context, AttributeSet attrs) {
 		super(context, attrs);
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		try {
+			return super.onInterceptTouchEvent(ev);
+		} catch (IllegalArgumentException e) {
+			Logger.w("Exception while zooming: " + e.toString());
+			return false;
+		}
 	}
 }
