d19059fd7dfe08eface16ea970c3ab401c1a5c35
Stefan Haertel <haertel.dev@gmail.com>
2015-11-11 10:58:58 +0100

    Add class for managing database changes

---
+++ app/src/main/java/de/eightbitboy/hijacr/data/DatabaseOpenHelper.java
@@ -0,0 +1,17 @@
+package de.eightbitboy.hijacr.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import de.eightbitboy.hijacr.data.dao.DaoMaster;
+
+public class DatabaseOpenHelper extends DaoMaster.OpenHelper {
+	public DatabaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
+		super(context, name, factory);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
+}
