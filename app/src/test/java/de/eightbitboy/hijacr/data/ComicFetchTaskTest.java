98c69e5708223dcf1e64769bf6c736ad023dfce2
Stefan Haertel <haertel.dev@gmail.com>
2015-11-26 11:37:21 +0100

    Sample test

--- app/src/test/java/de/eightbitboy/hijacr/data/ComicFetchTaskTest.java
+++ app/src/test/java/de/eightbitboy/hijacr/data/ComicFetchTaskTest.java
@@ -1,5 +1,22 @@
 package de.eightbitboy.hijacr.data;
 
-public class ComicFetchTaskTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import de.eightbitboy.hijacr.BuildConfig;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class ComicFetchTaskTest {
+	@Test
+	public void doSomething() {
+		String foo = "x";
+		String bar = "x";
+		Assert.assertThat(foo, equalTo(bar));
+	}
 }
