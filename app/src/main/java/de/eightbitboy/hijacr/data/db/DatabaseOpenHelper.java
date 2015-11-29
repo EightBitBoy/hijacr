44c26a2d2d7018b2ee5a0b9a1275f3b18b725d77
Stefan Haertel <haertel.dev@gmail.com>
2015-11-29 09:50:50 +0100

    Todo

--- app/src/main/java/de/eightbitboy/hijacr/data/db/DatabaseOpenHelper.java
+++ app/src/main/java/de/eightbitboy/hijacr/data/db/DatabaseOpenHelper.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 
 import de.eightbitboy.hijacr.data.dao.DaoMaster;
 
+//TODO use this for handling database changes
+//TODO implement this when creating a new table for saving reading progress
 public class DatabaseOpenHelper extends DaoMaster.OpenHelper {
 	public DatabaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
 		super(context, name, factory);
