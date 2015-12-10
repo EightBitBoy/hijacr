21d27013f943f504a65b49cb3e4fef6675b03013
Stefan Haertel <haertel.dev@gmail.com>
2015-12-10 21:48:58 +0100

    Add some debug code

--- app/src/main/java/de/eightbitboy/hijacr/activities/DebugActivity.java
+++ app/src/main/java/de/eightbitboy/hijacr/activities/DebugActivity.java
@@ -1,10 +1,14 @@
 package de.eightbitboy.hijacr.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import de.eightbitboy.hijacr.R;
 import de.eightbitboy.hijacr.Statistics;
+import de.eightbitboy.hijacr.fragments.dialog.ErrorDialog;
 
 public class DebugActivity extends AppCompatActivity {
 	@Override
@@ -14,5 +18,19 @@ public class DebugActivity extends AppCompatActivity {
 		Statistics.logView("Debug");
 
 		setContentView(R.layout.activity_debug);
+
+		Button doSomethingButton = (Button) findViewById(R.id.do_something_button);
+		doSomethingButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				doSomething();
+			}
+		});
+
+	}
+
+	private void doSomething() {
+		DialogFragment dialog = ErrorDialog.newInstance("Doing something!");
+		dialog.show(getSupportFragmentManager().beginTransaction(), "dialog");
 	}
 }
