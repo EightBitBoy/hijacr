de5a347fea16ca61584114f001a9099b7872548d
Stefan Haertel <haertel.dev@gmail.com>
2015-12-10 14:00:48 +0100

    Add new test

---
+++ app/src/test/java/de/eightbitboy/hijacr/data/FetchAllComicsTest.java
@@ -0,0 +1,12 @@
+package de.eightbitboy.hijacr.data;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import de.eightbitboy.hijacr.BuildConfig;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class FetchAllComicsTest {
+}
